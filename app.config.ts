a1a5ab9979725098343dcc713caa58deccf84a3b
Andrew <1191143+ndrut@users.noreply.github.com>
2025-05-21 19:25:15 -0500

    add prerender

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     preset: "cloudflare-pages",
-
+    compressPublicAssets: true,
+    prerender: {
+        crawlLinks: true
+    },
     rollupConfig: {
       external: ["node:async_hooks"]
     }

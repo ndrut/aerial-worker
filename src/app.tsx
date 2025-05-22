8bfbbec60afc7e2b27dff74ed7a1eb9fbbc9304f
Andrew <1191143+ndrut@users.noreply.github.com>
2025-05-21 19:26:01 -0500

    only set the title

--- src/app.tsx
+++ src/app.tsx
@@ -9,9 +9,7 @@ export default function App() {
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
+          <Title>aerials - n dru</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
